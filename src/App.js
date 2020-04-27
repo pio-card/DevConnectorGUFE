91d1ab795525adce7b75d45009d13880b4530180
pio-card <59215392+pio-card@users.noreply.github.com>
2020-04-26 22:04:38 -0700

    basic code base
    login, register, auth, alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
+
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar.js";
+import Landing from "./components/layout/Landing.js";
+import Alert from "./components/layout/Alert.js";
+
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+//bring redux to react
+import { Provider } from "react-redux";
+import store from "./store";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser(), []);
+  });
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+//2.4 - create a root reducer file - index.js
+import { combineReducers } from "redux";
+import alert from "./alert"; // RA-4.2
+import auth from "./auth"; // RA-4.2
+export default combineReducers({
+  //list reducers to pass as args to combineReducers function
+  alert,
+  auth
+});
